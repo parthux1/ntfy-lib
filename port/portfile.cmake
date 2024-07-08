2297d5716a1818b4e27907ffc611971f16bdeb28
Yann Arend Zankl <yann.a.zankl@gmail.com>
2024-07-08 02:30:04 +0200

    chore: match sha512 of tag v1.0.0

--- port/portfile.cmake
+++ port/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO parthux1/ntfy-lib
         REF v1.0.0
-        SHA512 0
+        SHA512 fc137699dddc3ecd29643ff1308ae2207c85e1ce5611951c68316a5489701d8b673f9835da495a0f4b85970f0740b28ee6e16155298ba3aeb957cd6a4d95f7b4
         HEAD_REF master
 )
 
